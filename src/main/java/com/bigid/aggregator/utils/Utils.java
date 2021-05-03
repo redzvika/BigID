6abfeaf2e3081a591da8e8f874b8189e04e46e94
zvika rubanenko <redzvika@gmail.com>
2021-05-03 18:57:32 +0300

    Adding some documentation

--- src/main/java/com/bigid/aggregator/utils/Utils.java
+++ src/main/java/com/bigid/aggregator/utils/Utils.java
@@ -5,13 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class Utils {
 
-
+    /**
+     * Transforms a very large string to lines.
+     * Adding absolute info ,line offset and character offset.
+     * @param bigLine
+     * @param linesStartPosition
+     * @param characterStartPosition
+     * @return
+     */
     public static List<Line> parseStringToLines(String bigLine,int linesStartPosition,int characterStartPosition){
         List<Line> lines=new ArrayList<>();
         List<String> rawLines=new ArrayList<>();
-
         int linePosition=0;
         for (int i=0;i<bigLine.length();i++){
             if (bigLine.charAt(i)=='\r' && bigLine.charAt(i+1)=='\n' && i+1<bigLine.length()){
@@ -19,7 +26,6 @@ public class Utils {
                 linePosition=i+2;
             }
         }
-
         int characterPosition=0;
         for (int i=0;i<rawLines.size();i++){
             Line line=new Line(i+linesStartPosition,characterPosition+characterStartPosition,rawLines.get(i));
